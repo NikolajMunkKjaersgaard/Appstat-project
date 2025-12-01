32e399acd7fb6bd7fe5f5b220ce884607362fab3
LauritsFauli <besluttede.raseri-0c@icloud.com>
2025-12-01 14:23:29 +0100

    python fil så man kan importere til sin notebook

---
+++ BallOnIncline_data.py
@@ -0,0 +1,42 @@
+gate1, sig_gate1 = [0.1631, 0.1631, 0.1631, 0.1635], [0.0001, 0.0005, 0.0005, 1e-3]
+gate2, sig_gate2 = [0.3404, 0.3403, 0.3407, 0.3405], [0.0001, 0.0005, 0.0005, 1e-3]
+gate3, sig_gate3 = [0.5216, 0.5218, 0.5218, 0.5218], [0.0001, 0.0005, 0.0005, 1e-3]
+gate4, sig_gate4 = [0.7120, 0.7120, 0.7119, 0.7121], [0.0001, 0.0005, 0.0005, 1e-3]
+gate5, sig_gate5 = [0.8872, 0.8879, 0.8872, 0.8875], [0.0003, 0.0005, 0.0005, 1e-3]
+
+ballS_d, sig_ballS_d = [9.99e-3, 10.00e-3,10.00e-3,9.97e-3], [0.5e-3, 1e-4,2e-5,1e-5]
+ballM_d, sig_ballM_d = [12.70e-3, 12.67e-3,12.71e-3,12.68e-3], [0.5e-3, 1e-4,2e-5,1e-5]
+ballL_d, sig_ballL_d = [14.99e-3, 15.00e-3,15.01e-3,14.99e-3], [0.5e-3, 1e-4,2e-5,1e-5]
+
+rail_width, sig_rail_width = [5.98e-3, 6.06e-3,6.04e-3, 5.89e-3], [0.5e-3, 1e-4,2e-5, 1e-5]
+base, sig_base = [0.9004, 0.9005, 0.9007, 0.9006], [0.0003, 1e-3, 1e-3, 0.0005]
+height, sig_height = [0.2236, 0.2217, 0.2224, 0.2221], [0.0002, 1e-3, 1e-3, 0.0005]
+rail_angle1, sig_rail_angle1 = [13.9, 14.09, 13.95, 13.75], [0.1, 1e-2, 0.1, 0.5]
+rail_angle2, sig_rail_angle2 = [13.7, 13.79, 13.65, 14.00], [0.1, 1e-2, 0.1, 0.5]
+
+filenames =[
+'Big_1.csv'
+'Big_1b.csv'
+'Big_2.csv'
+'Big_2b.csv'
+'Big_3.csv'
+'Big_3b.csv'
+'Big_4.csv'
+'Big_4b.csv'
+'Little_1.csv'
+'Little_1b.csv'
+'Little_2.csv'
+'Little_2b.csv'
+'Little_3.csv'
+'Little_3b.csv'
+'Little_4.csv'
+'Little_4b.csv'
+'Medium_1.csv'
+'Medium_1b.csv'
+'Medium_2.csv'
+'Medium_2b.csv'
+'Medium_3.csv'
+'Medium_3b.csv'
+'Medium_4.csv'
+'Medium_4b.csv'
+]
